13d042e0e4cd74a1aa82fc18c807f7f7ca532085
fampa101118 <108610320+fampa101118@users.noreply.github.com>
2022-07-22 20:49:17 +0530

    index.js

--- index.js
+++ index.js
@@ -10,17 +10,24 @@ function closeNav() {
 
 
 function myFunction() {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("mySearch");
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
-  ul = document.getElementById("myMenu");
-  li = ul.getElementsByTagName("li");
-  for (i = 0; i < li.length; i++) {
-    a = li[i].getElementsByTagName("a")[0];
-    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
-      li[i].style.display = "";
-    } else {
-      li[i].style.display = "none";
-    }
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
   }
 }
+
+
+
+
