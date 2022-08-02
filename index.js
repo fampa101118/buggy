726515eb992a620fc8760a4fd071f73dec7bf2c4
fampa101118 <108610320+fampa101118@users.noreply.github.com>
2022-08-02 13:11:05 +0530

    index.js

--- index.js
+++ index.js
@@ -31,3 +31,20 @@ function myFunction() {
 
 
 
+document.onreadystatechange = function() {
+            if (document.readyState !== "complete") {
+                document.querySelector(
+                  "body").style.visibility = "hidden";
+                document.querySelector(
+                  "#loader").style.visibility = "visible";
+            } else {
+                document.querySelector(
+                  "#loader").style.display = "none";
+                document.querySelector(
+                  "body").style.visibility = "visible";
+            }
+        };
+
+
+
+
